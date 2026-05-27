3b6c5b72c7b806246ac4115e9fe728a92c67ab28
rragupathi <rragupathi@griddynamics.com>
2026-05-28 00:45:29 +0530

    updated dependency for cloud

--- src/main/java/com/forge/talentAcquisitionEngine/candidateService/resumeParser/aiengine/service/ResumeStoreService.java
+++ src/main/java/com/forge/talentAcquisitionEngine/candidateService/resumeParser/aiengine/service/ResumeStoreService.java
@@ -9,7 +9,6 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.UUID;
