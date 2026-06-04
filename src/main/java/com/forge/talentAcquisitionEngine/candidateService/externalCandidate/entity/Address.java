145b37e8c726274c1b809a7e312f67df5e9395bd
rragupathi <rragupathi@griddynamics.com>
2026-06-04 17:10:53 +0530

    updated candidate

---
+++ src/main/java/com/forge/talentAcquisitionEngine/candidateService/externalCandidate/entity/Address.java
@@ -0,0 +1,41 @@
+package com.forge.talentAcquisitionEngine.candidateService.externalCandidate.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "candidate_id")
+    private ExternalCandidate candidate;
+
+    @NotNull(message = "Street1 cannot be null")
+    private String street1;
+
+    @NotNull(message = "Street2 cannot be null")
+    private String street2;
+
+    @NotNull(message = "City cannot be null")
+    private String city;
+
+    @NotNull(message = "State cannot be null")
+    private String state;
+
+    @NotNull(message = "Country cannot be null")
+    private String country;
+
+    @NotNull(message = "Zip code cannot be null")
+    @Size(min = 6, max = 6, message = "Zip code must be between 6 digits")
+    private Long zipCode;
+}
---
+++ src/main/java/com/forge/talentAcquisitionEngine/candidateService/externalCandidate/entity/ExperienceDetail.java
@@ -0,0 +1,39 @@
+package com.forge.talentAcquisitionEngine.candidateService.externalCandidate.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "experience_detail")
+@Getter
+@Setter
+public class ExperienceDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long experienceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "candidate_id")
+    private ExternalCandidate candidate;
+
+    @NotNull(message = "Company name cannot be null")
+    private String companyName;
+
+    @NotNull(message = "Job title cannot be null")
+    private String jobTitle;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    @NotNull(message = "Designation cannot be null")
+    private String designation;
+}
